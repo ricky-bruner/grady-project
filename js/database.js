a3f8f044acb2c63b238aa0c1b97943002563082e
ricky-bruner <alteredecho07@yahoo.com>
2018-08-04 11:38:47 -0500

    started datastructure

--- js/database.js
+++ js/database.js
@@ -1,1 +1,59 @@
 console.log("im here!");
+
+const stateQuarters = [
+    {
+        name: "Tennessee",
+        image: "http://www.your-rare-coin-collecting-guide.com/images/2002_1_tennessee.jpg",
+        year: "2002",
+    },
+    {
+        name: "New York",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4434-wctmb.jpg",
+        year: "2001",
+    },
+    {
+        name: "Mississippi",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4469-wctmb.jpg",
+        year: "2002",
+    },
+    {
+        name: "Hawaii",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4591.pr-wctmb.jpg",
+        year: "2008",
+    },
+    {
+        name: "Florida",
+        image: "http://www.your-rare-coin-collecting-guide.com/images/2004_2_florida.jpg",
+        year: "2004",
+    },
+    {
+        name: "Georgia",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4407-wctmb.jpg",
+        year: "1999",
+    },
+    {
+        name: "Texas",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4500-wctmb.jpg",
+        year: "2004",
+    },
+    {
+        name: "Washington",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4556-wctmb.jpg",
+        year: "2007",
+    },
+    {
+        name: "Utah",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4571.pr-wctmb.jpg",
+        year: "2007",
+    },
+    {
+        name: "California",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4512-wctmb.jpg",
+        year: "2005",
+    },
+    {
+        name: "Alaska",
+        image: "https://www.littletoncoin.com/wcsstore/LCC/images/catalog/200x310/4584-wctmb.jpg",
+        year: "2008",
+    }
+]
